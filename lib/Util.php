d69f98915fd5743f903ee4bd7036699848c311dc
vladykin <vladykin@897c843c-3922-0410-9e4d-05f83131bdfe>
2007-03-04 15:59:30 +0000

    - Specified 'public static' modifiers for all methods - Replaced is_a() with instanceof

--- lib/Util.php
+++ lib/Util.php
@@ -4,57 +4,48 @@
 
 class Util {
 
-    /**
-     * @access public static
-     */
-    function isValidId($id) {
+    public static function isValidId($id) {
         return is_integer($id) && $id >= 0
             || is_string($id) && preg_match('/^\d+$/', $id);
     }
 
-    function isValidName($name) {
+    public static function isValidName($name) {
         return is_string($name) 
             && $name{0} != '.'
             && preg_match('/^[a-z0-9_\.\-]+$/i', $name);
     }
 
-    /**
-     * @access public static
-     */
-    function isValidBoolean($bool) {
+    public static function isValidBoolean($bool) {
         return $bool === false || $bool === true
             || $bool === 0 || $bool === 1
             || $bool === '0' || $bool === '1';
     }
 
-    function isValidInteger($int) {
+    public static function isValidInteger($int) {
         return is_integer($int) 
             || is_string($int) && preg_match('/^[\+\-]?\d+$/', $int);
                 
     }
 
-    /**
-     * @access public static
-     */
-    function isValidDate($date) {
+    public static function isValidDate($date) {
         return is_string($date)
             && preg_match('/^\d{4}\-\d{2}\-\d{2}$/', $date);
     }
 
-    function isValidDateTime($datetime) {
+    public static function isValidDateTime($datetime) {
         return is_string($datetime)
             && preg_match('/^\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}$/', $datetime);
     }
 
-    function isValidEmail($email) {
+    public static function isValidEmail($email) {
         return is_string($email)
             && preg_match('/^[a-z0-9_-]+(\.[a-z0-9_-]+)*\@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,4}$/i', $email);
     }
     
-    function isArrayOf($array, $class) {
+    public static function isArrayOf($array, $class) {
         if (is_array($array)) {
             foreach ($array as $item) {
-                if (!is_a($item, $class)) {
+                if (!($item instanceof $class)) {
                     return false;
                 }
             }
@@ -64,7 +55,7 @@ class Util {
         }
     }
 
-    function packArray($array) {
+    public static function packArray($array) {
         assert('is_array($array)');
         $newarray = array();
         foreach ($array as $element) {
