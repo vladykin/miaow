992346891148c3eaec40f21cdc10c0ef8b21e1e2
vladykin <vladykin@897c843c-3922-0410-9e4d-05f83131bdfe>
2007-03-10 10:06:36 +0000

    - made getKnownTypes() read files in handlers/ directory - removed PHP4-style assign by reference ( &= ) - added 'public static' modifiers to methods

--- lib/HandlerFactory.php
+++ lib/HandlerFactory.php
@@ -8,8 +8,19 @@ class HandlerFactory {
      * @access public static
      * @return array
      */    
-    function getKnownTypes() {
-        return array('Text', 'List', 'News', 'Catalogue');
+    public static function getKnownTypes() {
+        static $knownTypes = array();
+        if (empty($knownTypes)) {
+            $dh = opendir(dirname(__FILE__) . '/handlers/');
+            while (($fn = readdir($dh)) !== false) {
+                if (substr($fn, -4) === '.php') {
+                    // trim 'Handler.php'
+                    $knownTypes[] = substr($fn, 0, -11);
+                }
+            }
+            closedir($dh);
+        }
+        return $knownTypes;
     }
 
     /**
@@ -17,7 +28,7 @@ class HandlerFactory {
      * @param string $nodeTypeName
      * @return Handler
      */
-    function &getHandler($nodeTypeName) {
+    public static function getHandler($nodeTypeName) {
         assert('is_string($nodeTypeName) && strlen($nodeTypeName) > 0');
         return HandlerFactory::createHandler($nodeTypeName . 'Handler');
     }
@@ -27,12 +38,12 @@ class HandlerFactory {
      * @param string $className
      * @return Handler
      */
-    function &createHandler($className) {
+    public static function createHandler($className) {
         assert('is_string($className) && strlen($className) > 0');
         static $instances = array();
         if (!array_key_exists($className, $instances)) {
             require_once(dirname(__FILE__) . "/handlers/$className.php");
-            $instances[$className] =& new $className();
+            $instances[$className] = new $className();
         }
         return $instances[$className];
     }
